153c74041c495afe1b343c486fa09bdbf50bcd6a
Carla Aldana <carliss.aldana@gmail.com>
2022-07-31 23:43:06 -0300

    cambios backend

--- Back end/src/main/java/com/portfolio/cya/controller/ExperienciaController.java
+++ Back end/src/main/java/com/portfolio/cya/controller/ExperienciaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://cyaldana-fe.web.app/")
 public class ExperienciaController {
     @Autowired
     ExperienciaService expServ;
